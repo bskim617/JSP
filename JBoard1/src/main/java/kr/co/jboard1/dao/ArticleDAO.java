c2882b56a974af332cd5929a611e9cc75026c44f
bskim617 <bskim617@gmail.com>
2022-11-01 13:39:33 +0900

    2011/11/01

--- JBoard1/src/main/java/kr/co/jboard1/dao/ArticleDAO.java
+++ JBoard1/src/main/java/kr/co/jboard1/dao/ArticleDAO.java
@@ -13,7 +13,7 @@ import kr.co.jboard1.db.DBCP;
 import kr.co.jboard1.db.Sql;
 
 public class ArticleDAO {
-
+	
 	private static ArticleDAO instance = new ArticleDAO();
 	public static ArticleDAO getInstance() {
 		return instance;
@@ -21,7 +21,105 @@ public class ArticleDAO {
 	private ArticleDAO() {}
 	
 	// 기본 CRUD
-	public void insertArticel() {}
+	public int insertArticle(ArticleBean article) {
+		int parent = 0;
+		try{
+			Connection conn = DBCP.getConnection();
+			
+			// 트랜젝션 시작
+			conn.setAutoCommit(false);
+			
+			Statement stmt = conn.createStatement();
+			PreparedStatement psmt = conn.prepareStatement(Sql.INSERT_ARTICLE);
+			
+			psmt.setString(1, article.getTitle());
+			psmt.setString(2, article.getContent());
+			psmt.setInt(3, article.getFname() == null ? 0 : 1);
+			psmt.setString(4, article.getUid());
+			psmt.setString(5, article.getRegip());
+			
+			psmt.executeUpdate(); // INSERT
+			ResultSet rs = stmt.executeQuery(Sql.SELECT_MAX_NO); // SELECT
+			
+			// 작업확정
+			conn.commit(); 
+			
+			if(rs.next()){
+				parent = rs.getInt(1);
+			}
+			
+			rs.close();
+			stmt.close();
+			psmt.close();
+			conn.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return parent;
+	}
+	
+	public void insertFile(int parent, String newName, String fname) {
+		try{
+			Connection conn = DBCP.getConnection();
+			PreparedStatement psmt = conn.prepareStatement(Sql.INSERT_FILE);
+			psmt.setInt(1, parent);
+			psmt.setString(2, newName);
+			psmt.setString(3, fname);
+			
+			psmt.executeUpdate();
+			psmt.close();
+			conn.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public ArticleBean insertComment(ArticleBean comment) {
+		
+		ArticleBean article = null;
+		
+		try{
+			Connection conn = DBCP.getConnection();
+			
+			// 트랜잭션 시작
+			conn.setAutoCommit(false);
+			
+			PreparedStatement psmt = conn.prepareStatement(Sql.INSERT_COMMENT);
+			Statement stmt = conn.createStatement();
+			
+			psmt.setInt(1, comment.getParent());
+			psmt.setString(2, comment.getContent());
+			psmt.setString(3, comment.getUid());
+			psmt.setString(4, comment.getRegip());
+			
+			psmt.executeUpdate();
+			ResultSet rs = stmt.executeQuery(Sql.SELECT_COMMENT_LATEST);
+			
+			// 작업확정
+			conn.commit();
+			
+			if(rs.next()) {
+				article = new ArticleBean();
+				article.setNo(rs.getInt(1));
+				article.setParent(rs.getInt(2));
+				article.setContent(rs.getString(6));
+				article.setRdate(rs.getString(11).substring(2, 10));
+				article.setNick(rs.getString(12));
+			}
+			
+			rs.close();
+			stmt.close();			
+			psmt.close();
+			conn.close();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return article;
+	}
 	
 	public ArticleBean selectArticle(String no) {
 		ArticleBean article = null;
@@ -30,7 +128,9 @@ public class ArticleDAO {
 			Connection conn = DBCP.getConnection();
 			PreparedStatement psmt = conn.prepareStatement(Sql.SELECT_ARTICLE);
 			psmt.setString(1, no);
+			
 			ResultSet rs = psmt.executeQuery();
+			
 			if(rs.next()){
 				article = new ArticleBean();
 				article.setNo(rs.getInt(1));
@@ -47,13 +147,11 @@ public class ArticleDAO {
 				article.setFno(rs.getInt(12));
 				article.setOriName(rs.getString(13));
 				article.setDownload(rs.getInt(14));
-				
 			}
 			
 			rs.close();
 			psmt.close();
 			conn.close();
-			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
@@ -61,9 +159,9 @@ public class ArticleDAO {
 		return article;
 	}
 	
-	public List<ArticleBean> selectArticels(int start) {
+	public List<ArticleBean> selectArticles(int start) {
 		
-		List<ArticleBean> articles = new ArrayList<>();
+		List<ArticleBean> articles = new ArrayList<>();	
 		
 		try{
 			Connection conn = DBCP.getConnection();
@@ -88,28 +186,28 @@ public class ArticleDAO {
 				article.setNick(rs.getString(12));
 				
 				articles.add(article);
-				
 			}
 			
 			rs.close();
 			psmt.close();
-			conn.close();
-			
+			conn.close();		
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+		
 		return articles;
 	}
 	
 	public FileBean selectFile(String fno) {
+		
 		FileBean fb = null;
 		
 		try{
-			Connection conn= DBCP.getConnection();
+			Connection conn = DBCP.getConnection();
 			PreparedStatement psmt = conn.prepareStatement(Sql.SELECT_FILE);
 			psmt.setString(1, fno);
 			
-			ResultSet rs= psmt.executeQuery();
+			ResultSet rs = psmt.executeQuery();
 			
 			if(rs.next()){
 				fb = new FileBean();
@@ -124,69 +222,104 @@ public class ArticleDAO {
 			rs.close();
 			psmt.close();
 			conn.close();
-			
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		return fb;
 	}
 	
-	public void updateArticel() {}
+	public List<ArticleBean> selectComments(String parent) {
+		
+		List<ArticleBean> comments = new ArrayList<>();
+		
+		try {
+			Connection conn = DBCP.getConnection();
+			PreparedStatement psmt = conn.prepareStatement(Sql.SELECT_COMMENTS);
+			psmt.setString(1, parent);
+			
+			ResultSet rs = psmt.executeQuery();
+			
+			while(rs.next()) {
+				ArticleBean comment = new ArticleBean();
+				comment.setNo(rs.getInt(1));
+				comment.setParent(rs.getInt(2));
+				comment.setComment(rs.getInt(3));
+				comment.setCate(rs.getString(4));
+				comment.setTitle(rs.getString(5));
+				comment.setContent(rs.getString(6));
+				comment.setFile(rs.getInt(7));
+				comment.setHit(rs.getInt(8));
+				comment.setUid(rs.getString(9));
+				comment.setRegip(rs.getString(10));
+				comment.setRdate(rs.getString(11).substring(2, 10));
+				comment.setNick(rs.getString(12));
+				comments.add(comment);
+			}
+			
+			rs.close();
+			psmt.close();
+			conn.close();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return comments;
+	}
 	
-	public void deleteArticel() {}
+	public void updateArticle() {}
+	public void deleteArticle() {}
 	
 	// 전체 게시물 카운트
 	public int selectCountTotal() {
+		
 		int total = 0;
 		
 		try {
-			Connection conn= DBCP.getConnection();
+			Connection conn = DBCP.getConnection();
 			Statement stmt = conn.createStatement();
 			
-			ResultSet rs= stmt.executeQuery((Sql.SELECT_COUNT_TOTAL));
+			ResultSet rs = stmt.executeQuery(Sql.SELECT_COUNT_TOTAL);
 			
 			if(rs.next()) {
 				total = rs.getInt(1);
 			}
 			rs.close();
 			stmt.close();
-			conn.close();
-					
-		}catch(Exception e) {
+			conn.close();		
+			
+		}catch (Exception e) {
 			e.printStackTrace();
 		}
-		return total;
+		
+		return total;		
 	}
 	
 	public void updateArticleHit(String no) {
 		try{
-			Connection conn= DBCP.getConnection();
+			Connection conn = DBCP.getConnection();
 			PreparedStatement psmt = conn.prepareStatement(Sql.UPDATE_ARTICLE_HIT);
 			psmt.setString(1, no);
-			
 			psmt.executeUpdate();
-			
+
 			psmt.close();
 			conn.close();
-			
 		}catch(Exception e){
-			e.printStackTrace();
+			 e.printStackTrace();
 		}
 	}
 	
-	public void updatefileDownload(String fno) {
+	public void updateFileDownload(String fno) {
 		try{
-			Connection conn= DBCP.getConnection();
+			Connection conn = DBCP.getConnection();
 			PreparedStatement psmt = conn.prepareStatement(Sql.UPDATE_FILE_DOWNLOAD);
 			psmt.setString(1, fno);
-			
 			psmt.executeUpdate();
-			
+
 			psmt.close();
 			conn.close();
-			
 		}catch(Exception e){
-			e.printStackTrace();
+			 e.printStackTrace();
 		}
 	}
 }
